d1a3164091c20377e3e181c775a1611d40075586
NikitaTysh <nikita.tyshchenko7@gmail.com>
2022-11-21 22:50:21 +0100

    task 2 done
    lib Vimeo
    localStor
    throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,16 @@
-
-console.log('asd')
+console.log('start file 2');
+const iframe = document.querySelector('iframe');
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+const player = new Player(iframe);
+player.on('play', () => {
+  if (localStorage.getItem('currentTime')) {
+    player.setCurrentTime(localStorage.getItem('currentTime'));
+  }
+});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('currentTime', data.seconds);
+  }, 1000)
+);
