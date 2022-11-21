eca172b3fdd4fe908867338d79d8e9964b174d63
NikitaTysh <nikita.tyshchenko7@gmail.com>
2022-11-21 20:46:08 +0100

    task 1 done (simplelightbox)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+const gallery = document.querySelector(".gallery");
+const elToAdd = galleryItems
+  .map(
+    (el) => `<li class="gallery__item">
+    <a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>
+  </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", elToAdd);
+console.log("asd");
+new SimpleLightbox(".gallery__item a", {
+  overlayOpacity: 0.5,
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
--- src/testjs.js
+++
@@ -1,8 +0,0 @@
-const ar = document.querySelector('textarea')
-
-console.log('adss')
-ar.addEventListener('input',throttle(fnc,1000))
-function fnc (e)
-{
-    console.log(e.target.value)
-}
