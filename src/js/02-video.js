54ca1c5275c98546fcf4c4c8efeb04a882d2867c
NikitaTysh <nikita.tyshchenko7@gmail.com>
2022-12-11 13:46:20 +0100

    test upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,13 @@
-console.log('start file 2');
+console.log('start file 223');
 const iframe = document.querySelector('iframe');
 import Player from '@vimeo/player';
 import  throttle  from 'lodash.throttle';
 const player = new Player(iframe);
+if (localStorage.getItem('currentTime')) {
+  player.setCurrentTime(localStorage.getItem('currentTime'));
+}
 player.on('play', () => {
+  console.log(`time: ${localStorage.getItem('currentTime')}`);
   if (localStorage.getItem('currentTime')) {
     player.setCurrentTime(localStorage.getItem('currentTime'));
   }
@@ -12,5 +16,6 @@ player.on(
   'timeupdate',
   throttle(function (data) {
     localStorage.setItem('currentTime', data.seconds);
+    console.log(`time: ${localStorage.getItem('currentTime')}`);
   }, 1000)
 );
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,29 @@
 console.log('start 3th file');
-import  throttle  from "lodash.throttle";
+import throttle from 'lodash.throttle';
 const input = document.querySelector('.feedback-form');
-const submit = input.querySelector('button');
+const submit = document.querySelector('.feedback-form button');
+let a;
 submit.addEventListener('click', onClick);
-console.dir(input);
 const localData = {};
 if (localStorage.getItem('data') !== null) {
   const parsedSettings = JSON.parse(localStorage.getItem('data'));
   input.elements.email.value = parsedSettings.email;
   input.elements.message.value = parsedSettings.message;
 }
-input.addEventListener('input', throttle(inputFunc, 500));
+input.addEventListener('input', throttle(inputFunc, 50));
 function inputFunc(e) {
   localData.email = e.currentTarget.elements.email.value;
   localData.message = e.currentTarget.elements.message.value;
   localStorage.setItem('data', JSON.stringify(localData));
 }
-function onClick() {
-//   input.email.value = '';
-//   input.message.value = '';
-  localStorage.clear();
+function onClick(e) {
+  e.preventDefault();
   input.reset();
-
-  
+  console.log(`email : ${JSON.parse(localStorage.getItem('data')).email}`);
+  console.log(`message : ${JSON.parse(localStorage.getItem('data')).message}`);
+  localStorage.clear();
 }
+
+
 // ментору -> подскажите как правильно очистить форму?
 // если буде 100 полей , не прописывать же каждый в ручную
