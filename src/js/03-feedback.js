f8994099ab22661c8baff2e98508448f97c806a6
NikitaTysh <nikita.tyshchenko7@gmail.com>
2022-12-12 14:57:53 +0100

    upd 3.03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,18 @@
-console.log('start 3.01');
+console.log('start 3.03');
 import throttle from 'lodash.throttle';
 const input = document.querySelector('.feedback-form');
 const submit = document.querySelector('.feedback-form button');
 submit.addEventListener('click', onClick);
-const localData = {};
-if (localStorage.getItem('data') !== null) {
-  const parsedSettings = JSON.parse(localStorage.getItem('data'));
-  input.elements.email.value = parsedSettings.email;
-  input.elements.message.value = parsedSettings.message;
+if (localStorage.getItem('data')) {
+  const {email,message} = JSON.parse(localStorage.getItem('data'));
+  input.elements.email.value = email || '';
+  input.elements.message.value = message || '';
 }
 input.addEventListener('input', throttle(inputFunc, 500));
 function inputFunc(e) {
-  console.dir();
-  if(e.target.localName==='input')
-  {
-    localData.email = e.target.value;
-    
-  }
-  else 
-  localData.message = e.target.value;
-  localStorage.setItem('data', JSON.stringify(localData));
+const userData = JSON.parse(localStorage.getItem('data')) || {}
+userData[e.target.name] = e.target.value;
+localStorage.setItem('data',JSON.stringify(userData))
 }
 function onClick(e) {
   e.preventDefault();
